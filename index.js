ad994ff6882e0834254e051fc6f0b4cfa31125f6
Simran Bhandari <simranbhandari036@gmail.com>
2024-11-16 12:55:54 +0530

    register containers on start events

--- index.js
+++ index.js
@@ -4,6 +4,42 @@ const Docker = require("dockerode");
 
 const docker = new Docker({ socketPath: "/var/run/docker.sock" });
 
+const db = new Map();
+
+docker.getEvents(function (err, stream) {
+    if(err){
+        console.log('Error in getting events', err);
+        return;
+    }
+
+    stream.on("data", async (chunk) => {
+        if(!chunk) return;
+        const event = JSON.parse(chunk.toString());
+
+        if(event.Type === 'container' && event.Action == 'start'){
+            const container = docker.getContainer(event.id);
+            const containerInfo = await container.inspect();
+
+            const containerName = containerInfo.Name.substring(1);
+            const ipAddress = containerInfo.NetworkSettings.IPAddress;
+
+            const exposedPort = Object.keys(containerInfo.Config.ExposedPorts);
+            let defaultPort = null;
+
+            if(exposedPort && exposedPort.length > 0){
+                const [port, type] = exposedPort[0].split("/");
+                if(type === "tcp"){
+                    defaultPort = port;
+                }
+            }
+            console.log(
+                `Registering ${containerName}.localhost --> http://${ipAddress}:${defaultPort}`
+            );
+            db.set(containerName, { containerName, ipAddress, defaultPort });
+        }
+    });
+})
+
 const managementAPI = express();
 
 managementAPI.use(express.json());
